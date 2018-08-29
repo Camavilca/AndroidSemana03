16f54ea93bcb0740cc418e135ee1acc587117315
Camavilca <ocamavilcachavez@gmail.com>
2018-08-29 00:30:17 -0500

    Primeros Cambios del segundo layout

---
+++ README.md
@@ -0,0 +1,1 @@
+# AndroidSemana03
---
+++ app/src/main/java/com/orlandocamavilca/musicapop/MainActivity.java
@@ -0,0 +1,46 @@
+package com.orlandocamavilca.musicapop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        String[] valores = {
+                "orlando.camavilca@tecsup.edu.pe",
+                "erick.fernandez@tecsup.edu.pe",
+                "diego.martinez@tecsup.edu.pe",
+                "alejandro.quesada@tecsup.edu.pe",
+                "leidy.callupe@tecsup.edu.pe",
+                "carlos.chavez@tecsup.edu.pe"};
+        spinner.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, valores));
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id)
+            {
+                Toast.makeText(adapterView.getContext(), (String) adapterView.getItemAtPosition(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent)
+            {
+                // vacio
+
+            }
+        });
+    }
+}
